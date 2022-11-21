bb5a9a8ed15e304980e7d38b54ea3de3f5dce488
Anastasia-Boroday <anastasiaborodaj22@gmail.com>
2022-11-21 20:58:53 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Player(iframeRef);
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const CurrentTime = localStorage.getItem(LOCALSTORAGE_KEY)
+console.log(LOCALSTORAGE_KEY);
+const onPlay = function (data) {
+  const timeUpdate = data.seconds;
+  console.log(timeUpdate);
+  localStorage.setItem(LOCALSTORAGE_KEY, timeUpdate);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+player.setCurrentTime(CurrentTime).then(function(seconds) {
+  
+}).catch(function(error) {
+  switch (error.name) {
+      case 'RangeError':
+          break;
+      default:
+          break;
+  }
+});
 
